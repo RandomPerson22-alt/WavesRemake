79fb0b2360fb3be4039d3fa168fd639512d512e7
randomperson22 <sabaswad79@gmail.com>
2025-12-17 23:36:55 -0500

    Update GameClient.kt

--- core/src/main/kotlin/com/randomperson22/wavesremake/client/GameClient.kt
+++ core/src/main/kotlin/com/randomperson22/wavesremake/client/GameClient.kt
@@ -1,29 +1,35 @@
 package com.randomperson22.wavesremake.client
 
-class GameClient(val host: String, val tcpPort: Int, val udpPort: Int) {
+class GameClient(val hostAddress: String, val tcpPort: Int = 54555, val udpPort: Int = 54777) {
     private val client = Client()
-    private val players = mutableMapOf<Int, PlayerClient>() // other players + self
 
     init {
+        client.kryo.apply {
+            register(RoomCreateRequest::class.java)
+            register(RoomCreateResponse::class.java)
+            register(RoomJoinRequest::class.java)
+            register(RoomJoinResponse::class.java)
+            register(PlayerState::class.java)
+        }
+
         client.start()
-        client.connect(5000, host, tcpPort, udpPort)
+        client.connect(5000, hostAddress, tcpPort, udpPort)
+
         client.addListener(object : Listener() {
             override fun received(connection: Connection, obj: Any) {
-                if (obj is PlayerState) {
-                    // update or create a PlayerClient for this state
-                    players[obj.id]?.state = obj
+                when(obj) {
+                    is RoomCreateResponse -> println("Room created: ${obj.roomCode}, you are host ${obj.hostId}")
+                    is RoomJoinResponse -> println("Join status: ${obj.success}, message: ${obj.message}")
                 }
             }
         })
     }
 
-    fun update(delta: Float) {
-        // send player input to server
-        players[myId]?.let { it.sendInput(currentInput) }
+    fun createRoom(playerName: String) {
+        client.sendTCP(RoomCreateRequest(playerName))
     }
 
-    fun render(batch: SpriteBatch) {
-        // render all players.
-        players.values.forEach { it.render(batch) }
+    fun joinRoom(roomCode: String, playerName: String) {
+        client.sendTCP(RoomJoinRequest(roomCode, playerName))
     }
 }
