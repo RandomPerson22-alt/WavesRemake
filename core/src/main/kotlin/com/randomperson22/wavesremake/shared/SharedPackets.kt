1872858d84b1d7824e27de708322bf1edb35fee9
randomperson22 <sabaswad79@gmail.com>
2026-01-17 22:58:33 -0500

    Fix file path comment in SharedPackets.kt

--- core/src/main/kotlin/com/randomperson22/wavesremake/shared/SharedPackets.kt
+++ core/src/main/kotlin/com/randomperson22/wavesremake/shared/SharedPackets.kt
@@ -1,4 +1,4 @@
-//WavesRemake: core/scr/main/kotlin/randomperson22/wavesremake/shared/SharedPackets.kt
+//WavesRemake: core/scr/main/kotlin/com/randomperson22/wavesremake/shared/SharedPackets.kt
 package com.randomperson22.wavesremake.shared
 
 import kotlinx.serialization.Serializable
